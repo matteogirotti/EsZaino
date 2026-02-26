954d129253a4b71721842eaef77b186363bd5dc5
10154 <matteogirotti10@gmail.com>
2026-02-26 08:04:42 +0100

    .

--- EsZaino/Program.cs
+++ EsZaino/Program.cs
@@ -9,23 +9,35 @@ namespace EsZaino
             int pesoOttimo = 0;
             List<int> soluzioneOttima = new List<int>();
             int[] valori = { 11, 13, 7, 21, 40 };
+
             if (valori.Length < 0)
+            {
                 throw new Exception("non ci sono valori");
+            }
             if (capacitaMassima <= 0)
+            {
                 throw new Exception("Capacità non valida!");
+            }
+            
             Zaino zaino = new Zaino(0, 0, new List<int>());
             Nodo<Zaino> radice = new Nodo<Zaino>(zaino);
-            Naviga(radice, valori, ref pesoOttimo, ref soluzioneOttima);
+
+            NavigaZaino(radice, valori, ref pesoOttimo, ref soluzioneOttima);
+
             Console.WriteLine($"Risultato ottimo: {pesoOttimo}");
             Console.Write("Oggetti Inclusi : ");
+
             foreach (int indice in soluzioneOttima)
+            {
                 Console.Write($"{valori[indice]} ");
+            }              
         }
 
-        static void Naviga(Nodo<Zaino> nodo, int[] valori, ref int pesoOttimo, ref List<int> soluzioneOttima)
+        static void NavigaZaino(Nodo<Zaino> nodo, int[] valori, ref int pesoOttimo, ref List<int> soluzioneOttima)
         {
             Zaino s = nodo.Dati;
-            if (s.IndiceUltimoOggetto >= valori.Length)     //finisco i valori
+
+            if (s.IndiceUltimoOggetto >= valori.Length)
             {
                 if (s.PesoAccumulato > pesoOttimo || (s.PesoAccumulato == pesoOttimo && s.OggettiPresi.Count < soluzioneOttima.Count))
                 {
@@ -34,19 +46,18 @@ namespace EsZaino
                 }
                 return;
             }
-            //Prendo l'oggetto
             int pesoConNuovoOggetto = s.PesoAccumulato + valori[s.IndiceUltimoOggetto];
+
             if (pesoConNuovoOggetto <= capacitaMassima)
             {
                 List<int> nuovaLista = new List<int>(s.OggettiPresi);
                 nuovaLista.Add(s.IndiceUltimoOggetto);
 
                 nodo.Prendo = new Nodo<Zaino>(new Zaino(s.IndiceUltimoOggetto + 1, pesoConNuovoOggetto, nuovaLista));
-                Naviga(nodo.Prendo, valori, ref pesoOttimo, ref soluzioneOttima);
+                NavigaZaino(nodo.Prendo, valori, ref pesoOttimo, ref soluzioneOttima);
             }
-            //Non prendo l'oggetto
             nodo.NonPrendo = new Nodo<Zaino>(new Zaino(s.IndiceUltimoOggetto + 1, s.PesoAccumulato, new List<int>(s.OggettiPresi)));
-            Naviga(nodo.NonPrendo, valori, ref pesoOttimo, ref soluzioneOttima);
+            NavigaZaino(nodo.NonPrendo, valori, ref pesoOttimo, ref soluzioneOttima);
         }
     }
 }
